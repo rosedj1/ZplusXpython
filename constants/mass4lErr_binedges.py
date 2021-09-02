5ee901a9590fd885d0d356b5b6a6c49638aceecf
Jake Rosenzweig <rosedj1@ufl.edu>
2021-09-02 16:38:56 -0400

    Made RelMassErrBinEdges class.

---
+++ constants/mass4lErr_binedges.py
@@ -0,0 +1,115 @@
+finalstate_dct = {
+    "4mu" : 1,
+    "4e" : 2,
+    "2e2mu" : 3,
+    "2mu2e" : 4
+}
+
+class RelMassErrBinEdges:
+
+    def __init__(self):
+        self.alphabet_indextup_dct = self.make_alphabet_to_binedge_index_dct()
+        self.mass4lErr_binedge_dct = {
+            # 4l final state code : bin edge values
+            # WARNING: Filippo is inconsistent about using final state codes!
+            # The keys below are 1 less than the typical values.
+            # Frequently '1' means '4mu' but now '0' means '4mu'.
+            0 : [          # Bin A is the
+                0.0000000, # <== 0th and
+                0.0056000, # <== 1st bin edges.
+                0.0061500,
+                0.0067125,
+                0.0072500,
+                0.0078000,
+                0.0084250,
+                0.0092375,
+                0.0105875,
+                0.3000000
+                ],
+            1 : [
+                0.0000000,
+                0.0109250,
+                0.0125250,
+                0.0141625,
+                0.0160250,
+                0.0181750,
+                0.0205750,
+                0.0232750,
+                0.0269500,
+                0.3000000
+                ],
+            2 : [
+                0.0000000,
+                0.0086250,
+                0.0097250,
+                0.0106500,
+                0.0116875,
+                0.0130500,
+                0.0148625,
+                0.0180500,
+                0.0225125,
+                0.3000000
+                ],
+            3 : [
+                0.0000000,
+                0.0078750,
+                0.0088125,
+                0.0097875,
+                0.0109250,
+                0.0122875,
+                0.0138250,
+                0.0156375,
+                0.0183250,
+                0.3000000
+                ],
+        }
+
+    def make_alphabet_to_binedge_index_dct(self, letters='A B C D E F G H I'):
+        """Return a dict like:
+            {
+                'A' : (0, 1),
+                'B' : (1, 2),
+                'C' : (2, 3),
+                ...
+            }
+        """
+        # Make alphabet keys.
+        alphabet_ls = letters.split()
+        # alphabet_ls = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split()
+        indices = list(range( len(alphabet_ls) + 1 ))
+        # Make values (2-elem tuple) by threading indices together.
+        ls_of_tup_of_indices = zip(indices[:-1], indices[1:])
+        return dict(zip(alphabet_ls, ls_of_tup_of_indices))
+
+    def get_edges_mass4lErr_bin(self, finalstate_dct, fs_str=None, fs_int=None, m4lErr_bin_code=""):
+        """
+        Return binedges (2-tup) corresponding to final state and relmasserr
+        code.
+        
+        Parameters
+        ----------
+        fs_str : str
+            Final state of 4-leptons: "4mu", "4e", "2e2mu", "2mu2e"
+        fs_int : int
+            The code corresponding to the final state:
+                "4mu"   : 1,
+                "4e"    : 2,
+                "2e2mu" : 3,
+                "2mu2e" : 4
+        m4lErr_bin_code : str
+            The code corresponding to the relative mass error bin.
+            Accepts: "A", "B", ..., "I"
+        """
+        using_str_code = fs_str is not None
+        using_int_code = fs_int is not None
+        assert not (using_str_code and using_int_code)
+        assert using_str_code or using_int_code
+        if using_str_code:
+            fs_int = finalstate_dct[fs_str]
+        else:
+            assert fs_int in (1, 2, 3, 4)
+        fs_int -= 1  # Must subtract 1 to match the weird keys.
+        all_binedges_for_fs = self.mass4lErr_binedge_dct[fs_int]
+        ndx1, ndx2 = self.alphabet_indextup_dct[m4lErr_bin_code]
+        binedge_tup = (all_binedges_for_fs[ndx1], all_binedges_for_fs[ndx2])
+        return binedge_tup
--- scripts/plotters/make_dists_m4l_Dkinbkgvsm4l.py
+++ scripts/plotters/make_dists_m4l_Dkinbkgvsm4l.py
@@ -1,88 +1,21 @@
-"""Draw distributions for RedBkg studies."""
+"""Draw distributions for RedBkg studies.
+
+Author: Jake Rosenzweig
+Created: 2021-08-30
+Updated: 2021-09-02
+"""
 import os
 import ROOT as rt
 # Local modules.
 from Utils_ROOT.ROOT_classes import make_TH1F, make_TH2F
 from Utils_ROOT.Printer import CanvasPrinter
 from Utils_Python.Utils_Files import check_overwrite
+from constants.mass4lErr_binedges import RelMassErrBinEdges, finalstate_dct
 
 # infile = "./data/ZLL_CR_FRapplied/Data.root"
 infile = "./data/ZLL_CR_FRapplied/Data_and_ZZ.root"
-outpath_pdf = "/blue/avery/rosedj1/zplusx_vukasin/ZplusXpython/plots/m4l_Dkinvsm4l/test/test13_data_and_zz_branchdict.pdf"
+outpath_pdf = "/blue/avery/rosedj1/ZplusXpython/plots/m4l_Dkinvsm4l/test/test13_data_and_zz_branchdict.pdf"
 overwrite = 1
-
-os.makedirs(os.path.dirname(outpath_pdf), exist_ok=True)
-check_overwrite(outpath_pdf, overwrite=overwrite)
-
-f = rt.TFile(infile, "read")
-t = f.Get("passedEvents")
-
-xlabel_m4l_refit = r"m_{4l}^{refit}"
-xlabel_m4l_vtxFSR_BS = r"m_{4l}^{FSR, VX}"
-xlabel_m4l_refit_vtx_BS = r"m_{4l}^{refit, VX}"
-formula_str = r"N_{SR}^{RedBkg} = "
-formula_str += r"#sum_{i}^{N_{3P1F}}#frac{f^{i}}{1-f^{i}} - "
-formula_str += r"w_{ZZ}#sum_{j}^{N_{3P1F}^{ZZ}}#frac{f^{j}}{1-f^{j}} - "
-formula_str += r"#sum_{k}^{N_{2P2F}}#left(#frac{f^{k}_{3}}{1-f^{k}_{3}}#right)"
-formula_str += r"#left(#frac{f^{k}_{4}}{1-f^{k}_{4}}#right)"
-
-def make_hist_draw_tree(branch, hist_internal_name, n_bins=70,
-                        xlabel=None, x_min=105, x_max=140,
-                        ylabel=None, y_min=None, y_max=None,
-                        weight=1):
-    h = make_TH1F(internal_name=hist_internal_name, title=formula_str, n_bins=n_bins,
-                xlabel=xlabel, x_min=x_min, x_max=x_max,
-                ylabel=ylabel, y_min=y_min, y_max=y_max, units="GeV")
-    # tex = rt.TLatex()
-    # tex.SetNDC()
-    # tex.SetTextSize(0.03)
-    # tex.SetTextColor(rt.kRed)
-    bin_beg = h.FindBin(x_min*1.001)
-    bin_end = h.FindBin(x_max*0.999)
-    t.Draw(f"{branch}>>{hist_internal_name}", weight, "hist")
-    integ = h.Integral(bin_beg, bin_end)
-    # tex.DrawLatex(0.6, 0.7, f"Integral = {integ:.1f}")
-    print_str = (
-        f"branch = {branch:<25}, x_min = {x_min:<13}, x_max = {x_max:<13}, integral = {integ:.6f}"
-    )
-    print(print_str)
-    printer.canv.Print(outpath_pdf)
-
-# h_eventWeight = make_TH1F(internal_name="h_eventWeight", title="eventWeight", n_bins=100,
-#               xlabel="eventWeight", x_min=-1, x_max=3,
-#               ylabel=None, units=None)
-# h_mass4lREFIT_wide = make_TH1F(internal_name="h_mass4lREFIT_wide", title=formula_str, n_bins=250,
-#               xlabel=xlabel_m4l_refit, x_min=70, x_max=570,
-#               ylabel=None, units="GeV")
-# h_mass4lREFIT_higgswindow = make_TH1F(internal_name="h_mass4lREFIT_higgswindow", title=formula_str, n_bins=70,
-#               xlabel=xlabel_m4l_refit, x_min=105, x_max=140,
-#               ylabel=None, units="GeV")
-# h_mass4lREFIT_higgswindow_narrow = make_TH1F(internal_name="h_mass4lREFIT_higgswindow_narrow", title=formula_str, n_bins=24,
-#               xlabel=xlabel_m4l_refit, x_min=118, x_max=130,
-#               ylabel=None, units="GeV")
-# h_mass4l_vtxFSR_BS_higgswindow = make_TH1F(internal_name="h_mass4l_vtxFSR_BS_higgswindow", title=formula_str, n_bins=70,
-#               xlabel=xlabel_m4l_vtxFSR_BS, x_min=105, x_max=140,
-#               ylabel=None, units="GeV")
-# h_mass4l_vtxFSR_BS_higgswindow_narrow = make_TH1F(internal_name="h_mass4l_vtxFSR_BS_higgswindow_narrow", title=formula_str, n_bins=24,
-#               xlabel=xlabel_m4l_vtxFSR_BS, x_min=118, x_max=130,
-#               ylabel=None, units="GeV")
-# h_mass4lREFIT_vtx_BS_higgswindow = make_TH1F(internal_name="h_mass4lREFIT_vtx_BS_higgswindow", title=formula_str, n_bins=70,
-#               xlabel=xlabel_m4l_vtxFSR_BS, x_min=105, x_max=140,
-#               ylabel=None, units="GeV")
-# h_mass4lREFIT_vtx_BS_higgswindow_narrow = make_TH1F(internal_name="h_mass4lREFIT_vtx_BS_higgswindow_narrow", title=formula_str, n_bins=24,
-#               xlabel=xlabel_m4l_vtxFSR_BS, x_min=118, x_max=130,
-#               ylabel=None, units="GeV")
-# h2 = make_TH2F(internal_name="h2", title="", 
-#           n_binsx=70, x_label=xlabel_m4l_refit, x_units="GeV", x_min=105, x_max=140,
-#           n_binsy=100, y_label=r"D_{bkg}^{kin}", y_units=None, y_min=0, y_max=1,
-#           z_min=None, z_max=None, z_label_size=None,
-#           n_contour=100)
-
-printer = CanvasPrinter(show_plots=False, show_statsbox=1, canv=None)
-printer.canv.Print(outpath_pdf + "[")
-# Draw distributions.
-weight = "is3P1F*weight_fr - is3P1F_zz*weight_fr - is2P2F*weight_fr"
-weight_alwayspos = f"({weight}) * ({weight}>=0)"  # 
 
 branch_dct = {
     # m4l.
@@ -106,24 +39,138 @@ branch_dct = {
     # "D_bkg_kin_vtx_BS" : r"m_{4l}",
 }
 
-for br in branch_dct.keys():
-    if ("mass4l" in br) and not ("Err" in br):
-        make_hist_draw_tree(branch=br, hist_internal_name=f"h_{br}_wide", n_bins=125, xlabel=branch_dct[br], x_min=70, x_max=570, ylabel=None, y_min=0.0, y_max=10.0, weight=weight_alwayspos)
-for br in branch_dct.keys():
-    if ("mass4l" in br) and not ("Err" in br):
-        make_hist_draw_tree(branch=br, hist_internal_name=f"h_{br}_higgswindow", n_bins=70, xlabel=branch_dct[br], x_min=105, x_max=140, ylabel=None, y_min=0.0, y_max=3.0, weight=weight_alwayspos)
-for br in branch_dct.keys():
-    if ("mass4l" in br) and not ("Err" in br):
-        make_hist_draw_tree(branch=br, hist_internal_name=f"h_{br}_higgswindow_narrow", n_bins=24, xlabel=branch_dct[br], x_min=118, x_max=130, ylabel=None, y_min=0.0, y_max=3.0, weight=weight_alwayspos)
+os.makedirs(os.path.dirname(outpath_pdf), exist_ok=True)
+check_overwrite(outpath_pdf, overwrite=overwrite)
+
+f = rt.TFile(infile, "read")
+t = f.Get("passedEvents")
+
+xlabel_m4l_refit = r"m_{4l}^{refit}"
+xlabel_m4l_vtxFSR_BS = r"m_{4l}^{FSR, VX}"
+xlabel_m4l_refit_vtx_BS = r"m_{4l}^{refit, VX}"
+formula_str = r"N_{SR}^{RedBkg} = "
+formula_str += r"#sum_{i}^{N_{3P1F}}#frac{f^{i}}{1-f^{i}} - "
+formula_str += r"w_{ZZ}#sum_{j}^{N_{3P1F}^{ZZ}}#frac{f^{j}}{1-f^{j}} - "
+formula_str += r"#sum_{k}^{N_{2P2F}}#left(#frac{f^{k}_{3}}{1-f^{k}_{3}}#right)"
+formula_str += r"#left(#frac{f^{k}_{4}}{1-f^{k}_{4}}#right)"
+
+def make_hist_draw_tree(branch, hist_internal_name, n_bins=70,
+                        xlabel=None, x_min=105, x_max=140,
+                        ylabel=None, y_min=None, y_max=None,
+                        weight=1):
+    h = make_TH1F(internal_name=hist_internal_name, title=formula_str, n_bins=n_bins,
+                xlabel=xlabel, x_min=x_min, x_max=x_max,
+                ylabel=ylabel, y_min=y_min, y_max=y_max, units="GeV")
+        # tex = rt.TLatex()
+        # tex.SetNDC()
+        # tex.SetTextSize(0.03)
+        # tex.SetTextColor(rt.kRed)
+        # tex.DrawLatex(0.6, 0.7, f"Integral = {integ:.1f}")
+    bin_end = h.FindBin(x_max * 0.99999)
+    bin_beg = h.FindBin(x_min * 1.00001)
+    t.Draw(f"{branch}>>{hist_internal_name}", weight, "hist")
+    integ = h.Integral(bin_beg, bin_end)
+    print_str = (
+        f"branch = {branch:<25}, x_min = {x_min:<13}, x_max = {x_max:<13}, integral = {integ:.6f}"
+    )
+    print(print_str)
+    printer.canv.Print(outpath_pdf)
+
+printer = CanvasPrinter(show_plots=False, show_statsbox=1, canv=None)
+printer.canv.Print(outpath_pdf + "[")
+
+class RedBkgEstimator:
+    """A class to make hists, rootfiles, and fits of reducible background."""
+
+    def __init__(self, finalstate_dct, fs, mass4lErr_bin_str):
+        """
+        TODO: Finish docstring.
+
+        Parameters
+        ----------
+        fs : str
+            Final state of 4l. Must be one of:
+            '4e', '4mu', '2e2mu', '2mu2e'
+        mass4lErr_bin_str : str
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
+        """
+        assert fs in finalstate_dct.keys()
+        self.fs = fs
+        self.fs_code = finalstate_dct[self.fs]
+        self.mass4lErr_bin_str = mass4lErr_bin_str
+        # Make a bin edge manager to parse bins, because it's confusing.
+        binedge_boi = RelMassErrBinEdges()
+        self.binedge_tup = binedge_boi.get_edges_mass4lErr_bin(
+            finalstate_dct, fs_str=fs, fs_int=None, m4lErr_bin_code=mass4lErr_bin_str
+            )
+
+    def make_hist_name(self, branch, dim=1):
+        """Return an internal name of hist specific to this estimator."""
+        return f"h{dim}_{branch}_{self.fs}_{self.mass4lErr_bin_str}"
+
+    def make_cut_str(self):
+        """Return a string of all cuts applied per event."""
+        weight = "is3P1F*weight_fr - is3P1F_zz*weight_fr - is2P2F*weight_fr"
+        weight_gteq_zero = f"{weight} >= 0"
+        fs_cut = f"finalState == {self.fs_code}"
+        m4lErr_cut = f"{self.binedge_tup[0]} <= mass4lErrREFIT_vtx_BS < {self.binedge_tup[1]}"
+        # formula_alwayspos = f"({weight}) * ({weight}>=0)"
+        return f"({weight}) && ({weight_gteq_zero}) && ({fs_cut}) && ({m4lErr_cut})"
+
+    def make_hist(self, tree, branch, title=None, n_bins=70,
+                  xlabel=None, x_min=105, x_max=140,
+                  ylabel=None, y_min=None, y_max=None, units=None,
+                  draw_hist=True, opt=""):
+        """
+        Return a filled TH1F from the events in `tree`, `branch` with
+        `cuts` applied.
+        """
+        cuts = self.make_cut_str()
+        internal_name = self.make_hist_name(branch, dim=1)
+        h = make_TH1F(internal_name=internal_name, title=title, n_bins=n_bins,
+                      xlabel=xlabel, x_min=x_min, x_max=x_max,
+                      ylabel=ylabel, y_min=y_min, y_max=y_max, units=units)
+        if not draw_hist:
+            opt = "goff"
+        tree.Draw(f"{branch}>>{internal_name}", cuts, f"hist {opt}")
+        return h
+
+rbe = RedBkgEstimator(finalstate_dct=finalstate_dct, fs="4mu", mass4lErr_bin_str="B")
+
+# RESUME HERE.
+bin_end = h.FindBin(x_max * 0.99999)
+bin_beg = h.FindBin(x_min * 1.00001)
+integ = h.Integral(bin_beg, bin_end)
+print_str = (
+    f"branch = {branch:<25}, x_min = {x_min:<13}, x_max = {x_max:<13}, integral = {integ:.6f}"
+)
+print(print_str)
+
+tree, branch, title=None, n_bins=70,
+                  xlabel=None, x_min=105, x_max=140,
+                  ylabel=None, y_min=None, y_max=None, units=None,
+                  draw_hist=True, opt=""
+
+rbe.hist_ls = [rbe.make_hist(tree=t, branch=br, cuts=weight_alwayspos) for br in branch_dct.keys()]
+rbe.make_hist(tree=t, branch="mass4l_vtxFSR_BS")
+rbe.make_hist(tree=t, branch="mass4lREFIT_vtx_BS")
+rbe.make_hist2d(branch_y="D_bkg_kin", branch_x="mass4lREFIT_vtx_BS", normalize_cols=True)
+rbe.fit_m4l(hist=rbe.h_mass4lREFIT_vtx_BS, draw=True, canv=printer.canv)
+rbe.save_rootfile(outpath="")
+# for br in branch_dct.keys():
+#     if ("mass4l" in br) and not ("Err" in br):
+#         make_hist_draw_tree(branch=br, hist_internal_name=f"h_{br}_wide", n_bins=125, xlabel=branch_dct[br], x_min=70, x_max=570, ylabel=None, y_min=0.0, y_max=10.0, weight=weight_alwayspos)
+# for br in branch_dct.keys():
+#     if ("mass4l" in br) and not ("Err" in br):
+#         make_hist_draw_tree(branch=br, hist_internal_name=f"h_{br}_higgswindow", n_bins=70, xlabel=branch_dct[br], x_min=105, x_max=140, ylabel=None, y_min=0.0, y_max=3.0, weight=weight_alwayspos)
+# for br in branch_dct.keys():
+#     if ("mass4l" in br) and not ("Err" in br):
+#         make_hist_draw_tree(branch=br, hist_internal_name=f"h_{br}_higgswindow_narrow", n_bins=24, xlabel=branch_dct[br], x_min=118, x_max=130, ylabel=None, y_min=0.0, y_max=3.0, weight=weight_alwayspos)
 
 
 # make_hist_draw_tree(branch="mass4lREFIT_vtx_BS", hist_internal_name="h_mass4lREFIT_vtx_BS_wide", n_bins=125, xlabel=xlabel_m4l_vtxFSR_BS, x_min=70, x_max=570, ylabel=None, y_min=None, y_max=None, weight=weight_alwayspos)
 # make_hist_draw_tree(branch="mass4lREFIT_vtx_BS", hist_internal_name="h_mass4lREFIT_vtx_BS_higgswindow", n_bins=70, xlabel=xlabel_m4l_vtxFSR_BS, x_min=105, x_max=140, ylabel=None, y_min=0.0, y_max=None, weight=weight_alwayspos)
 # make_hist_draw_tree(branch="mass4lREFIT_vtx_BS", hist_internal_name="h_mass4lREFIT_vtx_BS_higgswindow_narrow", n_bins=24, xlabel=xlabel_m4l_vtxFSR_BS, x_min=118, x_max=130, ylabel=None, y_min=0.0, y_max=None, weight=weight_alwayspos)
-
-
-# t.Draw("eventWeight>>h_eventWeight", "eventWeight", "hist")
-# printer.canv.Print(outpath_pdf)
 
 # t.Draw("D_bkg_kin:mass4lREFIT>>h2", weight_alwayspos, "colz1")
 # printer.canv.Print(outpath_pdf)
