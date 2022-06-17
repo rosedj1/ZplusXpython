b6e2b65487daea669ae0cb2898108ecf166669a0
Jake Rosenzweig <rosedj1@ufl.edu>
2022-06-16 21:17:30 -0400

    Fixing setup_lxplus.sh

--- setup_lxplus.sh
+++ setup_lxplus.sh
@@ -1,3 +1,5 @@
+dirabove=$( dirname ${PWD} )
+export PYTHONPATH=${PYTHONPATH}:${PWD}
+export PYTHONPATH=${PYTHONPATH}:"${dirabove}/HiggsMassMeasurement"
 export PYTHONPATH=${PYTHONPATH}:'/afs/cern.ch/work/d/drosenzw/zplusx/ZplusXpython'
 export PYTHONPATH=${PYTHONPATH}:'/afs/cern.ch/work/d/drosenzw/HiggsMassMeasurement'
-export PYTHONPATH=${PYTHONPATH}:${PWD}
